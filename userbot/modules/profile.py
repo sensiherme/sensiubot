7a748fb8b071e69e40065e831d7f8c27848a02f3
AyiinXd <96849897+AyiinXd@users.noreply.github.com>
2022-03-16 19:03:37 +0800

    Update profile.py

--- userbot/modules/profile.py
+++ userbot/modules/profile.py
@@ -181,153 +181,25 @@ async def remove_profilepic(delpfp):
     await delpfp.edit(f"`Berhasil Menghapus {len(input_photos)} Foto Profil.`")
 
 
-@ayiin_cmd(pattern="info(?: |$)(.*)")
-async def who(event):
-    xx = await edit_or_reply(event, "`Mengambil Informasi Data User ini...`")
-    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-    replied_user = await get_user(event)
-    try:
-        photo, caption = await fetch_info(replied_user, event)
-    except AttributeError:
-        return xx.edit("`Saya Tidak Mendapatkan Informasi Pengguna.`")
-    message_id_to_reply = event.message.reply_to_msg_id
-    if not message_id_to_reply:
-        message_id_to_reply = None
-    try:
-        await event.client.send_file(
-            event.chat_id,
-            photo,
-            caption=caption,
-            link_preview=False,
-            force_document=False,
-            reply_to=message_id_to_reply,
-            parse_mode="html",
-        )
-        if not photo.startswith("http"):
-            os.remove(photo)
-        await event.delete()
-    except TypeError:
-        await xx.edit(caption, parse_mode="html")
-
-
-async def get_user(event):
-    if event.reply_to_msg_id and not event.pattern_match.group(1):
-        previous_message = await event.get_reply_message()
-        replied_user = await event.client(
-            GetFullUserRequest(previous_message.sender_id)
-        )
-    else:
-        user = event.pattern_match.group(1)
-
-        if user.isnumeric():
-            user = int(user)
-
-        if not user:
-            self_user = await event.client.get_me()
-            user = self_user.id
-
-        if event.message.entities is not None:
-            probable_user_mention_entity = event.message.entities[0]
-
-            if isinstance(
-                    probable_user_mention_entity,
-                    MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user
-        try:
-            user_object = await event.client.get_entity(user)
-            replied_user = await event.client(GetFullUserRequest(user_object.id))
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-
-    return replied_user
-
-
-async def fetch_info(replied_user, event):
-    replied_user_profile_photos = await event.client(
-        GetUserPhotosRequest(
-            user_id=replied_user.user.id, offset=42, max_id=0, limit=80
-        )
-    )
-    replied_user_profile_photos_count = "This gay has no pic."
-    try:
-        replied_user_profile_photos_count = replied_user_profile_photos.count
-    except AttributeError:
-        pass
-    user_id = replied_user.user.id
-    first_name = replied_user.user.first_name
-    last_name = replied_user.user.last_name
-    try:
-        dc_id, _ = get_input_location(replied_user.profile_photo)
-    except Exception as e:
-        dc_id = "Tidak Dapat Mengambil DC ID!"
-        str(e)
-    common_chat = replied_user.common_chats_count
-    username = replied_user.user.username
-    user_bio = replied_user.about
-    is_bot = replied_user.user.bot
-    restricted = replied_user.user.restricted
-    verified = replied_user.user.verified
-    photo = await event.client.download_profile_photo(
-        user_id, TEMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg", download_big=True
-    )
-    first_name = (
-        first_name.replace("\u2060", "")
-        if first_name
-        else ("Orang Ini Tidak Punya Nama Depan")
-    )
-    last_name = (
-        last_name.replace("\u2060", "")
-        if last_name
-        else ("Orang Ini Tidak Punya Nama Belakang")
-    )
-    username = (
-        "@{}".format(username)
-        if username
-        else ("Pengguna Ini Tidak Menggunakan Username")
-    )
-    user_bio = "Orang Ini Tidak Menggunakan Bio" if not user_bio else user_bio
-
-    caption = "<b>Informasi Pengguna:</b>\n\n"
-    caption += f"Nama Depan: {first_name}\n"
-    caption += f"Nama Belakang: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Total Foto Profil: {replied_user_profile_photos_count}\n"
-    caption += f"Apakah Bot: {is_bot}\n"
-    caption += f"Dibatasi: {restricted}\n"
-    caption += f"Diverifikasi Oleh Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Obrolan Umum Untuk Pengguna Ini: {common_chat}\n"
-    caption += "Link Permanen Ke Profil: "
-    caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
-
-    return photo, caption
 
 
 CMD_HELP.update(
     {
-        "profil": "**Plugin : **`profil`\
-        \n\n  •  **Syntax :** `.username` <username baru>\
+        "profil": f"**Plugin : **`profil`\
+        \n\n  •  **Syntax :** `{cmd}username` <username baru>\
         \n  •  **Function : **Menganti Username Telegram.\
-        \n\n  •  **Syntax :** `.name` <nama depan> atau `.name` <Nama Depan> <Nama Belakang>\
+        \n\n  •  **Syntax :** `{cmd}name` <nama depan> atau `{cmd}name` <Nama Depan> <Nama Belakang>\
         \n  •  **Function : **Menganti Nama Telegram Anda.\
-        \n\n  •  **Syntax :** `.setbio` <bio baru>\
+        \n\n  •  **Syntax :** `{cmd}setbio` <bio baru>\
         \n  •  **Function : **Untuk Mengganti Bio Telegram.\
-        \n\n  •  **Syntax :** `.setpfp`\
-        \n  •  **Function : **Balas Ke Gambar Ketik .setpfp Untuk Mengganti Foto Profil Telegram.\
-        \n\n  •  **Syntax :** `.delpfp` atau `.delpfp` <berapa profil>/<all>\
+        \n\n  •  **Syntax :** `{cmd}setpfp`\
+        \n  •  **Function : **Balas Ke Gambar Ketik {cmd}setpfp Untuk Mengganti Foto Profil Telegram.\
+        \n\n  •  **Syntax :** `{cmd}delpfp` atau `{cmd}delpfp` <berapa profil>/<all>\
         \n  •  **Function : **Menghapus Foto Profil Telegram.\
-        \n\n  •  **Syntax :** `.reserved`\
+        \n\n  •  **Syntax :** `{cmd}reserved`\
         \n  •  **Function : **Menunjukkan nama pengguna yang dipesan oleh Anda.\
-        \n\n  •  **Syntax :** `.count`\
+        \n\n  •  **Syntax :** `{cmd}count`\
         \n  •  **Function : **Menghitung Grup, Chat, Bot dll.\
-        \n\n  •  **Syntax :** `.info` <username> Atau Balas Ke Pesan Ketik `.data`\
-        \n  •  **Function : **Mendapatkan Informasi Pengguna.\
     "
     }
 )
